c81bcc7cdf7a1b2d337e2a090e1fd36b659c4f9f
saifullah-10 <saifullah.bus@gmail.com>
2024-03-27 04:47:23 +0600

    update chart

--- src/Root.jsx
+++ src/Root.jsx
@@ -5,10 +5,12 @@ import { createContext, useEffect, useState } from "react";
 export const BooksContext = createContext();
 export const ReadBook = createContext();
 export const Wishlist = createContext();
+export const ChartData = createContext();
 function Root() {
   const [readBook, setReadBook] = useState([]);
   const [wishList, setWishList] = useState([]);
   const [booksData, setBooksData] = useState([]);
+  const [chart, setChart] = useState([]);
   useEffect(() => {
     fetch("/books.json")
       .then((res) => res.json())
@@ -17,18 +19,20 @@ function Root() {
   console.log(readBook);
   return (
     <>
-      <Wishlist.Provider value={[wishList, setWishList]}>
-        <ReadBook.Provider value={[readBook, setReadBook]}>
-          <section className="max-w-[1440px] w-[98%] lg:w-[80%] mx-auto mt-3 lg:mt-5 sticky top-0 z-20">
-            <Navbar></Navbar>
-          </section>
-          <BooksContext.Provider value={booksData}>
-            <section className="max-w-[1440px] w-[98%] lg:w-[80%] mx-auto lg:mt-5 lg:min-h-[calc(100vh-111px)]">
-              <Outlet></Outlet>
+      <ChartData.Provider value={[chart, setChart]}>
+        <Wishlist.Provider value={[wishList, setWishList]}>
+          <ReadBook.Provider value={[readBook, setReadBook]}>
+            <section className="max-w-[1440px] w-[98%] lg:w-[80%] mx-auto mt-3 lg:mt-5 sticky top-0 z-20">
+              <Navbar></Navbar>
             </section>
-          </BooksContext.Provider>
-        </ReadBook.Provider>
-      </Wishlist.Provider>
+            <BooksContext.Provider value={booksData}>
+              <section className="max-w-[1440px] w-[98%] lg:w-[80%] mx-auto lg:mt-5 lg:min-h-[calc(100vh-111px)]">
+                <Outlet></Outlet>
+              </section>
+            </BooksContext.Provider>
+          </ReadBook.Provider>
+        </Wishlist.Provider>
+      </ChartData.Provider>
     </>
   );
 }
--- src/components/PageToRead/PageToRead.jsx
+++ src/components/PageToRead/PageToRead.jsx
@@ -8,10 +8,10 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import PropTypes from "prop-types";
+import { useContext, useEffect } from "react";
+import { ChartData } from "../../Root";
 const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
-
-const getFromLocalStorage = localStorage.getItem("bookList");
-const data = JSON.parse(getFromLocalStorage);
+let data = [];
 const getPath = (x, y, width, height) => {
   return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${
     y + height / 3
@@ -35,32 +35,53 @@ TriangleBar.propTypes = {
   width: PropTypes.any,
   height: PropTypes.any,
 };
-export default function PageToRead() {
+const PageToRead = () => {
+  const getFromLocalStorage = localStorage.getItem("bookList");
+  const convertedData = JSON.parse(getFromLocalStorage);
+
+  const [chart, setChart] = useContext(ChartData);
+  useEffect(() => {
+    setChart(convertedData);
+  }, []);
+  data = chart;
   return (
-    <ResponsiveContainer width={"100%"} height={500}>
-      <BarChart
-        data={data}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="bookName" />
-        <YAxis />
-        <Bar
-          dataKey="totalPages"
-          fill="#8884d8"
-          shape={<TriangleBar />}
-          label={{ position: "top" }}
-        >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-          ))}
-        </Bar>
-      </BarChart>
-    </ResponsiveContainer>
+    <>
+      {data ? (
+        <ResponsiveContainer width={"100%"} height={500}>
+          <BarChart
+            data={data}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="bookName" />
+            <YAxis />
+            <Bar
+              dataKey="totalPages"
+              fill="#8884d8"
+              shape={<TriangleBar />}
+              label={{ position: "top" }}
+            >
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              ))}
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      ) : (
+        <>
+          <div className="flex justify-center min-h-[calc(100vh-111px)] items-center">
+            <div>
+              <p className="text-xl font-medium">You Do not Read Any Book</p>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
-}
+};
+export default PageToRead;
