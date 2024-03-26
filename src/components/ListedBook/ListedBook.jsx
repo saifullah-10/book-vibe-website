4c6712fe5c1cd89f549ee789a438edeca007d6bb
saifullah-10 <saifullah.bus@gmail.com>
2024-03-26 09:47:41 +0600

    implimenting sorting algorithm

--- src/components/ListedBook/ListedBook.jsx
+++ src/components/ListedBook/ListedBook.jsx
@@ -3,16 +3,53 @@ import Readbook from "./Readbook";
 import { ReadBook, Wishlist } from "../../Root";
 
 const ListedBook = () => {
-  const [readbook] = useContext(ReadBook);
+  const [readbook, setReadBook] = useContext(ReadBook);
   const [wishBooks] = useContext(Wishlist);
+  const sortByRating = () => {
+    const sort = [].concat(readbook).sort((a, b) => b.rating - a.rating);
+    setReadBook(sort);
+  };
+  const sortByPage = () => {
+    const sort = []
+      .concat(readbook)
+      .sort((a, b) => b.totalPages - a.totalPages);
+    setReadBook(sort);
+  };
+  const sortByYear = () => {
+    const sort = []
+      .concat(readbook)
+      .sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+    setReadBook(sort);
+    console.log(sort);
+  };
+  console.log(readbook);
   return (
     <div>
       <div className="w-full text-center lg:py-8 bg-slate-200 rounded-xl">
         <h2>Books</h2>
       </div>
-      <div></div>
       <div>
-        <div role="tablist" className="tabs tabs-lifted">
+        <ul className=" menu lg:menu-horizontal bg-base-200 rounded-box">
+          <li>
+            <details>
+              <summary>Sort By</summary>
+              <ul className="z-10">
+                <li className="w-[200px]" onClick={sortByYear}>
+                  <a>By Publishing Year</a>
+                </li>
+                <li onClick={sortByPage}>
+                  <a> By Pages</a>
+                </li>
+                <li onClick={sortByRating}>
+                  <a>By Rating</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <div role="tablist" className="tabs tabs-lifted -z-10">
           <input
             type="radio"
             name="my_tabs_2"
